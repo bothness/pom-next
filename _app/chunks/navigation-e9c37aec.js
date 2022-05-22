df13147616da50abfcd926060e958cacb88a66a4
Ahmad Barclay <abarclay.ons@gmail.com>
2022-05-22 01:36:56 +0100

    Updates

---
+++ _app/chunks/navigation-e9c37aec.js
@@ -0,0 +1,1 @@
+import{c as a}from"./singletons-1b661bce.js";a.disable_scroll_handling;const t=a.goto;a.invalidate;a.prefetch;a.prefetch_routes;a.before_navigate;const o=a.after_navigate;export{o as a,t as g};
