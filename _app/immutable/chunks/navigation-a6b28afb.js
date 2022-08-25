5d5e2c42a6ee350e1af7ff6eb2fa6cd578be3c71
Ahmad Barclay <bothness@users.noreply.github.com>
2022-08-25 22:24:22 +0300

    Updates

--- _app/immutable/chunks/navigation-a6b28afb.js
+++
@@ -1,1 +0,0 @@
-import{c as a}from"./singletons-822f6284.js";a.disable_scroll_handling;const t=a.goto;a.invalidate;a.prefetch;a.prefetch_routes;a.before_navigate;const o=a.after_navigate;export{o as a,t as g};
--- _app/immutable/pages/index@redirect.svelte-2fa4bc9a.js
+++
@@ -1,1 +0,0 @@
-import{S as s,i as t,s as n}from"../chunks/index-3c7b3c2d.js";class o extends s{constructor(e){super(),t(this,e,null,null,n,{})}}export{o as default};
