5ca4ee1ccfede2a8121542094619d5a57a02ef97
Ahmad Barclay <bothness@users.noreply.github.com>
2023-05-06 14:49:26 +0100

    minor fix

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -132,7 +132,7 @@ export function getCookie(name) {
 	const nameEQ = name + "=";
 	const ca = document.cookie.split(";");
 	for (let i = 0; i < ca.length; i ++) {
-			const c = ca[i];
+			let c = ca[i];
 			while (c.charAt(0) === " ") c = c.substring(1,c.length);
 			if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length,c.length);
 	}
