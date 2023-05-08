bcdaaed87603f4c2666a0d6f8ad3c750885f078e
Majd Al-Shihabi <majd.al@gmail.com>
2023-05-08 12:49:09 -0400

    fetch layers and sheets locally

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -8,8 +8,10 @@ export async function load({ fetch }) {
 	const config = dev ? {method: "get", headers: {origin: "http://localhost:5173"}} : {method: "get"};
 	
 	let places = await getPlaces(locs_url, fetch);
-	let layers = await getLayers(new Request(layers_url, config), fetch);
-	let sheets = await getSheets(new Request(sheets_url, config), layers, fetch);
+// 	let layers = await getLayers(new Request(layers_url, config), fetch);
+// 	let sheets = await getSheets(new Request(sheets_url, config), layers, fetch);
+	let layers = await getLayers(layers_url, fetch);
+	let sheets = await getSheets(sheets_url, layers, fetch);
 
 	return { places, layers, sheets };
 }
