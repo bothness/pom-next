7163ac3e07f078897b44f3ebfa5576513351ce5e
Ahmad Barclay <bothness@users.noreply.github.com>
2023-07-17 22:59:28 +0100

    use pom apis for layers and sheets

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -5,11 +5,7 @@ import { locs_url, layers_url, sheets_url } from "$lib/config";
 import { getPlaces, getLayers, getSheets } from "$lib/utils";
 
 export async function load({ fetch }) {
-	const config = dev ? {method: "get", headers: {origin: "http://localhost:5173"}} : {method: "get"};
-	
 	let places = await getPlaces(locs_url, fetch);
-// 	let layers = await getLayers(new Request(layers_url, config), fetch);
-// 	let sheets = await getSheets(new Request(sheets_url, config), layers, fetch);
 	let layers = await getLayers(layers_url, fetch);
 	let sheets = await getSheets(sheets_url, layers, fetch);
 
